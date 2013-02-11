4cc8a8e9c76c6afa5d94557c15b643e17bc10d6b
cldavid <david.cluytens@gmail.com>
2013-02-11 01:51:37 +0100

    Added GPL Headers

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ aquacc
 ======
 
 Aquarium Control Center
+
+Web interface for my DIY dosing unit (ds_motor).
+
+David Cluytes <david.cluytens@gmail.com>
--- www/index.php
+++ www/index.php
@@ -1,4 +1,22 @@
-<?php 
+<?php
+/*
+ * This file is part of Aquarium Control Center (aquacc).
+ *
+ * Copyright (C) 2012-2013 Cluytens David <david.cluytens@gmail.com>
+ *
+ * Aquarium Control Center (aquacc) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * Aquarium Control Center (aquacc) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Aquarium Control Center (aquacc). If not, see <http://www.gnu.org/licenses/>.
+ */
 	require "dosingunit.php";
 ?>
 <html>
