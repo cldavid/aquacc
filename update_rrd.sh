ac36037cdb7a0e92f53e48b41317bec00d7a354a
David Cluytens <david.cluytens@gmail.com>
2014-05-15 22:03:36 +0200

    Create rrd / update rrd Todo Do a lookup on fixed temperature sensors. Since the place of each sensor in the rrd is fixed update is done position wise

---
+++ update_rrd.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+RRDPATH=/home/pi/aquacc
+GRAPHPATH=/var/www/rrd
+RAWCOLOUR="#FF0000"
+TRENDCOLOUR="#0000FF"
+
+mkdir -p $GRAPHPATH
+
+#hour
+rrdtool graph $GRAPHPATH/mhour.png --start -6h \
+		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
+		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
+		CDEF:intrend=intemp,1200,TREND \
+		CDEF:outtrend=outtemp,1200,TREND \
+		LINE2:intemp$RAWCOLOUR:"Inside temperature" \
+		LINE1:intrend$TRENDCOLOUR:"20 min average" \
+		LINE2:outtemp$RAWCOLOUR:"Outside temperature" \
+		LINE1:outtrend$TRENDCOLOUR:"20 min average"
+
+#day
+rrdtool graph $GRAPHPATH/mday.png --start -1d \
+		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
+		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
+		CDEF:intrend=intemp,1800,TREND \
+		CDEF:outtrend=outtemp,1800,TREND \
+		LINE2:intemp$RAWCOLOUR:"Inside temperature" \
+		LINE1:intrend$TRENDCOLOUR:"1h min average" \
+		LINE2:outtemp$RAWCOLOUR:"Outside temperature" \
+		LINE1:outtrend$TRENDCOLOUR:"1h min average"
+#week
+rrdtool graph $GRAPHPATH/mweek.png --start -1w \
+		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
+		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
+		LINE2:intemp$RAWCOLOUR:"Inside temperature" \
+		LINE2:outtemp$RAWCOLOUR:"Outside temperature" \
+
+#month
+rrdtool graph $GRAPHPATH/mmonth.png --start -1m \
+		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
+		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
+		LINE2:intemp$RAWCOLOUR:"Inside temperature" \
+		LINE2:outtemp$RAWCOLOUR:"Outside temperature" \
+
+#year
+rrdtool graph $GRAPHPATH/myear.png --start -1y \
+		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
+		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
+		LINE2:intemp$RAWCOLOUR:"Inside temperature" \
+		LINE2:outtemp$RAWCOLOUR:"Outside temperature" 
