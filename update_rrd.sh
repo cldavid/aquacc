4eb3bbd2b0016d728c81febd7f0ac732eda680d4
David Cluytens <david.cluytens@gmail.com>
2014-05-25 12:42:59 +0200

    min / avg / max lines

--- update_rrd.sh
+++ update_rrd.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 RRDPATH=/home/pi/aquacc
 GRAPHPATH=/var/www/rrd
-RAWCOLOUR1="#FF0000"
-RAWCOLOUR2="#00FF00"
-TRENDCOLOUR="#0000FF"
+RAWCOLOUR1="#00FF00"
+RAWCOLOUR2="#0000FF"
+MINCOLOUR="#FFFF00"
+AVGCOLOUR="#FF9900"
+MAXCOLOUR="#FF0000"
 
 mkdir -p $GRAPHPATH
 
@@ -11,47 +13,55 @@ mkdir -p $GRAPHPATH
 rrdtool graph $GRAPHPATH/mhour.png --start -6h\
 		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
 		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
-		CDEF:intrend=intemp,1800,TREND \
-		CDEF:outtrend=outtemp,1800,TREND \
 		GPRINT:intemp:LAST:"Current temp.\: %.2lf°C" \
 		LINE2:intemp$RAWCOLOUR1:"Inside temperature" \
-		LINE1:intrend$TRENDCOLOUR:"30 min average" \
 		GPRINT:outtemp:LAST:"Current temp.\: %.2lf°C" \
-		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" \
-		LINE1:outtrend$TRENDCOLOUR:"30 min average" 
+		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" 
 
 rrdtool graph $GRAPHPATH/big_mhour.png --start -6h -w 1024 -h 300\
 		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
 		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
-		CDEF:intrend=intemp,1800,TREND \
-		CDEF:outtrend=outtemp,1800,TREND \
-		GPRINT:intemp:LAST:"Current temp.\: %.2lf°C" \
+		VDEF:min_intemp=intemp,MINIMUM \
+		VDEF:min_outtemp=outtemp,MINIMUM \
+		VDEF:avg_intemp=intemp,AVERAGE \
+		VDEF:avg_outtemp=outtemp,AVERAGE \
+		VDEF:max_intemp=intemp,MAXIMUM \
+		VDEF:max_outtemp=outtemp,MAXIMUM \
+		HRULE:min_intemp$MINCOLOUR:"Min inside temperature" \
+		HRULE:min_outtemp$MINCOLOUR:"Min outside temperature\n" \
+		HRULE:avg_intemp$AVGCOLOUR:"Average inside temperature" \
+		HRULE:avg_outtemp$AVGCOLOUR:"Average outside temperature\n" \
+		HRULE:max_intemp$MAXCOLOUR:"Max inside temperature" \
+		HRULE:max_outtemp$MAXCOLOUR:"Max outside temperature\n" \
 		LINE2:intemp$RAWCOLOUR1:"Inside temperature" \
-		LINE1:intrend$TRENDCOLOUR:"30 min average" \
-		GPRINT:outtemp:LAST:"Current temp.\: %.2lf°C" \
+		GPRINT:intemp:LAST:"Current temp.\: %.2lf°C\n" \
 		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" \
-		LINE1:outtrend$TRENDCOLOUR:"30 min average" 
+		GPRINT:outtemp:LAST:"Current temp.\: %.2lf°C\n" 
 
 #day
 rrdtool graph $GRAPHPATH/mday.png --start -1d\
 		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
 		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
-		CDEF:intrend=intemp,1800,TREND \
-		CDEF:outtrend=outtemp,1800,TREND \
 		LINE2:intemp$RAWCOLOUR1:"Inside temperature" \
-		LINE1:intrend$TRENDCOLOUR:"1h min average" \
-		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" \
-		LINE1:outtrend$TRENDCOLOUR:"1h min average"
+		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" 
 
 rrdtool graph $GRAPHPATH/big_mday.png --start -1d  -w 1024 -h 300\
 		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
 		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
-		CDEF:intrend=intemp,1800,TREND \
-		CDEF:outtrend=outtemp,1800,TREND \
+		VDEF:min_intemp=intemp,MINIMUM \
+		VDEF:min_outtemp=outtemp,MINIMUM \
+		VDEF:avg_intemp=intemp,AVERAGE \
+		VDEF:avg_outtemp=outtemp,AVERAGE \
+		VDEF:max_intemp=intemp,MAXIMUM \
+		VDEF:max_outtemp=outtemp,MAXIMUM \
+		HRULE:min_intemp$MINCOLOUR:"Min inside temperature" \
+		HRULE:min_outtemp$MINCOLOUR:"Min outside temperature" \
+		HRULE:avg_intemp$AVGCOLOUR:"Average inside temperature" \
+		HRULE:avg_outtemp$AVGCOLOUR:"Average outside temperature" \
+		HRULE:max_intemp$MAXCOLOUR:"Max inside temperature" \
+		HRULE:max_outtemp$MAXCOLOUR:"Max outside temperature" \
 		LINE2:intemp$RAWCOLOUR1:"Inside temperature" \
-		LINE1:intrend$TRENDCOLOUR:"1h min average" \
-		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" \
-		LINE1:outtrend$TRENDCOLOUR:"1h min average"
+		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" 
 #week
 rrdtool graph $GRAPHPATH/mweek.png --start -1w \
 		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
@@ -62,6 +72,18 @@ rrdtool graph $GRAPHPATH/mweek.png --start -1w \
 rrdtool graph $GRAPHPATH/big_mweek.png --start -1w -w 1024 -h 300\
 		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
 		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
+		VDEF:min_intemp=intemp,MINIMUM \
+		VDEF:min_outtemp=outtemp,MINIMUM \
+		VDEF:avg_intemp=intemp,AVERAGE \
+		VDEF:avg_outtemp=outtemp,AVERAGE \
+		VDEF:max_intemp=intemp,MAXIMUM \
+		VDEF:max_outtemp=outtemp,MAXIMUM \
+		HRULE:min_intemp$MINCOLOUR:"Min inside temperature" \
+		HRULE:min_outtemp$MINCOLOUR:"Min outside temperature" \
+		HRULE:avg_intemp$AVGCOLOUR:"Average inside temperature" \
+		HRULE:avg_outtemp$AVGCOLOUR:"Average outside temperature" \
+		HRULE:max_intemp$MAXCOLOUR:"Max inside temperature" \
+		HRULE:max_outtemp$MAXCOLOUR:"Max outside temperature" \
 		LINE2:intemp$RAWCOLOUR1:"Inside temperature" \
 		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" 
 
@@ -72,9 +94,21 @@ rrdtool graph $GRAPHPATH/mmonth.png --start -1m\
 		LINE2:intemp$RAWCOLOUR1:"Inside temperature" \
 		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" 
 
-rrdtool graph $GRAPHPATH/big_mmonth.png --start -1m -w 1024 -h 300\
+		rrdtool graph $GRAPHPATH/big_mmonth.png --start -1m -w 1024 -h 300\
 		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
 		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
+		VDEF:min_intemp=intemp,MINIMUM \
+		VDEF:min_outtemp=outtemp,MINIMUM \
+		VDEF:avg_intemp=intemp,AVERAGE \
+		VDEF:avg_outtemp=outtemp,AVERAGE \
+		VDEF:max_intemp=intemp,MAXIMUM \
+		VDEF:max_outtemp=outtemp,MAXIMUM \
+		HRULE:min_intemp$MINCOLOUR:"Min inside temperature" \
+		HRULE:min_outtemp$MINCOLOUR:"Min outside temperature" \
+		HRULE:avg_intemp$AVGCOLOUR:"Average inside temperature" \
+		HRULE:avg_outtemp$AVGCOLOUR:"Average outside temperature" \
+		HRULE:max_intemp$MAXCOLOUR:"Max inside temperature" \
+		HRULE:max_outtemp$MAXCOLOUR:"Max outside temperature" \
 		LINE2:intemp$RAWCOLOUR1:"Inside temperature" \
 		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" 
 
@@ -88,5 +122,17 @@ rrdtool graph $GRAPHPATH/myear.png --start -1y\
 rrdtool graph $GRAPHPATH/big_myear.png --start -1y -w 1024 -h 300\
 		DEF:intemp=$RRDPATH/multirPItemp.rrd:in_temp:AVERAGE \
 		DEF:outtemp=$RRDPATH/multirPItemp.rrd:out_temp:AVERAGE \
+		VDEF:min_intemp=intemp,MINIMUM \
+		VDEF:min_outtemp=outtemp,MINIMUM \
+		VDEF:avg_intemp=intemp,AVERAGE \
+		VDEF:avg_outtemp=outtemp,AVERAGE \
+		VDEF:max_intemp=intemp,MAXIMUM \
+		VDEF:max_outtemp=outtemp,MAXIMUM \
+		HRULE:min_intemp$MINCOLOUR:"Min inside temperature" \
+		HRULE:min_outtemp$MINCOLOUR:"Min outside temperature" \
+		HRULE:avg_intemp$AVGCOLOUR:"Average inside temperature" \
+		HRULE:avg_outtemp$AVGCOLOUR:"Average outside temperature" \
+		HRULE:max_intemp$MAXCOLOUR:"Max inside temperature" \
+		HRULE:max_outtemp$MAXCOLOUR:"Max outside temperature" \
 		LINE2:intemp$RAWCOLOUR1:"Inside temperature" \
 		LINE2:outtemp$RAWCOLOUR2:"Outside temperature" 
