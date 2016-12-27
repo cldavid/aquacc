68620063da6363b7f0430e838f0908c731c495ef
David Cluytens <david.cluytens@softathome.com>
2016-12-27 21:33:39 +0100

    refactoring, helpers.js

--- www/js/aquacc.js
+++ www/js/aquacc.js
@@ -1,56 +1,9 @@
 $.getScript( "js/dsu.js", page_loaded);
 $.getScript( "js/pdu.js", page_loaded);
 $.getScript( "js/rrd.js", page_loaded);
+$.getScript( "js/helpers.js", page_loaded);
 $.getScript( "js/init.js", page_loaded);
 
 function page_loaded(data, textStatus, jqxhr) {
-	//console.log(data); // Data returned
-	console.log(textStatus); // Success
-	console.log(jqxhr.status); // 200
-	console.log("Load was performed.");
-}
-
-function stripLeadingZeroes(input) {
-	if((input.length > 1) && (input.substr(0,1) == "0"))
-		return input.substr(1);
-	else
-		return input;
-}
-
-function getDateString(d) {
-	var mday	= d.getDate();
-	var month = d.getMonth()+1;
-
-	if(mday.toString().length == 1) mday = '0' + mday;
-	if(month.toString().length == 1) month = '0' + month;
-
-	var curDate =  d.getFullYear() + '-' + month  + '-' + mday;
-	return curDate;
-}
-
-function getTimeString(d) {
-	var h	= d.getHours();
-	var m = d.getMinutes()+1;
-
-	if(h.toString().length == 1) h = '0' + h;
-	if(m.toString().length == 1) m = '0' + m;
-
-	var curTime =  h + ':' + m;
-	return curTime;
-}
-
-function cMinutes2TimeString(minutes) {
-	var h = Math.floor(minutes / 60);
-	var m = minutes - (h * 60);
-	if(h.toString().length == 1) h = '0' + h;
-	if(m.toString().length == 1) m = '0' + m;
-	return(h +':' + m);
-}
-
-function cTimeString2Minutes(timeString) {
-	var t = timeString.split(":");
-	var h = t[0];
-	var m = t[1];
-	m = parseInt(m, 10) + (parseInt(h, 10) * 60);
-	return m;
+	console.log("Load was performed (" + textStatus + ")");
 }
---
+++ www/js/helpers.js
@@ -0,0 +1,44 @@
+function stripLeadingZeroes(input) {
+	if((input.length > 1) && (input.substr(0,1) == "0"))
+		return input.substr(1);
+	else
+		return input;
+}
+
+function getDateString(d) {
+	var mday	= d.getDate();
+	var month = d.getMonth()+1;
+
+	if(mday.toString().length == 1) mday = '0' + mday;
+	if(month.toString().length == 1) month = '0' + month;
+
+	var curDate =  d.getFullYear() + '-' + month  + '-' + mday;
+	return curDate;
+}
+
+function getTimeString(d) {
+	var h	= d.getHours();
+	var m = d.getMinutes()+1;
+
+	if(h.toString().length == 1) h = '0' + h;
+	if(m.toString().length == 1) m = '0' + m;
+
+	var curTime =  h + ':' + m;
+	return curTime;
+}
+
+function cMinutes2TimeString(minutes) {
+	var h = Math.floor(minutes / 60);
+	var m = minutes - (h * 60);
+	if(h.toString().length == 1) h = '0' + h;
+	if(m.toString().length == 1) m = '0' + m;
+	return(h +':' + m);
+}
+
+function cTimeString2Minutes(timeString) {
+	var t = timeString.split(":");
+	var h = t[0];
+	var m = t[1];
+	m = parseInt(m, 10) + (parseInt(h, 10) * 60);
+	return m;
+}
