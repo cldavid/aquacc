22e56247971e733a70653b696b5e17065fd35b32
David Cluytens <David.Cluytens@gmail.com>
2017-12-06 23:57:03 +0100

    set correct number of rows x5

--- init_rrd.sh
+++ init_rrd.sh
@@ -2,8 +2,8 @@
 rrdtool create /www/multirPItemp.rrd  --step 60 \
 			DS:in_temp:GAUGE:600:-30:50 \
 			DS:out_temp:GAUGE:600:-30:50 \
-			RRA:AVERAGE:0.5:1:12 \
-			RRA:AVERAGE:0.5:1:288 \
-			RRA:AVERAGE:0.5:12:168 \
-			RRA:AVERAGE:0.5:12:720 \
-			RRA:AVERAGE:0.5:1440:365
+			RRA:AVERAGE:0.5:1:60 \
+			RRA:AVERAGE:0.5:1:1440 \
+			RRA:AVERAGE:0.5:12:840 \
+			RRA:AVERAGE:0.5:12:3600 \
+			RRA:AVERAGE:0.5:288:1825
